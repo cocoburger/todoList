44d39f97affa15ae56718e0912e1aa93705725c8
cocoburger <73986381+cocoburger@users.noreply.github.com>
2022-04-05 00:28:29 +0900

    REST API 연습.

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("test") //리소스
+public class TestController {
+
+	@GetMapping
+	public String testController() {
+		return "hello world";
+	}
+
+	@GetMapping("/testGetMapping")
+	public String testControllerWithPath() {
+		return "세상아 내가 나간다. 길을 비켜라";
+	}
+
+	@GetMapping("/{id}")
+	public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+		return "세상이 그리 넓다는 제가 한번 가보죠 No." + id;
+	}
+
+	@GetMapping("/testRequestParam")
+	public String testControllerRequestParam(@RequestParam(required = false) int id) {
+		return "여보세요. 내가 몇 번째 전화 거는 줄 알아?" + id;
+
+	}
+}
