740bea0a27f91836d85e750acdde6e52ab1c3e7a
cocoburger <73986381+cocoburger@users.noreply.github.com>
2022-04-05 23:35:45 +0900

    RequestEntity 추가. TodoList 개발을 위한 컨트롤러, 서비스 생성

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test") //리소스
 public class TestController {
-
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World ID " + testRequestBodyDTO.getId() + "Message : " +
+				testRequestBodyDTO.getMessage();
+	}
 	@GetMapping
 	public String testController() {
 		return "hello world";
@@ -28,8 +37,21 @@ public class TestController {
 
 	}
 
-	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
-		return "Hello World ID " + testRequestBodyDTO.getId() + "Message : " +
-				testRequestBodyDTO.getMessage();
+	@GetMapping("/testResponseBody")
+	public ResponseDTO<String> testControllerResponseBody() {
+		List<String> list = new ArrayList<>();
+		list.add("안녕하세요. 반가워요 잘지내요 다시 만나요~");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		response.getError();
+		return response;
+	}
+
+	@GetMapping("/testResponseEntity")
+	public ResponseEntity<?> testControllerResponseEntity() {
+		List<String> list = new ArrayList<>();
+		list.add("가즈아~~ 떡상 ㅎㅎㅎㅎ");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return  ResponseEntity.ok().body(response);
+	//	return  ResponseEntity.badRequest().body(response);
 	}
 }
