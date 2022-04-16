3a3914f2a420b90823a80e59f0dac5968a04de4f
cocoburger <73986381+cocoburger@users.noreply.github.com>
2022-04-16 22:57:58 +0900

    검색 api 추가

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,16 +1,17 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -18,12 +19,68 @@ import java.util.List;
 public class TodoController {
 	
 	//@RequiredArgsConstructor lombok 어노테이션을 사용하면 더 편리
-	
 	private final TodoService todoService;
 
 	public TodoController(TodoService todoService) {
 		this.todoService = todoService;
 	}
 
+	/**
+	 * @RequestBody를 사용하는 목적은 return value가 기본 자료형이 아닌 object처럼 복잡한 자료형을 요청에 보내고 싶은 경우에 사용한다.
+	 * @param dto
+	 * @return
+	 */
+	@PostMapping
+	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+		try {
+			String temporaryUserId = "temporary-user"; // temporary user id.
+
+			// (1) TodoEntity로 변환한다.
+			/**
+			 * todoEntity에는 id,title,done이 멤버변수로 있다.
+			 */
+			TodoEntity entity = TodoDTO.todoEntity(dto);
+
+			//(2) id를 null로 초기화한다. 생성 당시에는 id가 없어야 하기 때문이다.
+			entity.setId(null);
+
+			//(3) 임시 사용자 아이디를 설정해준다. 이 부분은 4장 인증과 인가에서 수정할 예정이다. 지금은 인증과 인가기능이 없으므로 한 사용자(temporary-user)만 로그인 없이 사용할 수 있는 애플리케이션인 셈이다.
+			entity.setUserId(temporaryUserId);
+
+			//(4) 서비스를 이용해 todo 엔티티를 생성한다.
+			List<TodoEntity> entities = todoService.create(entity);
+
+			//(5) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+			// (6) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다.
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+			// (7) ResponseDTO를 리턴한다.
+			return ResponseEntity.ok().body(response);
+
+		} catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
+	}
+
+	@GetMapping
+	public ResponseEntity<?> retrieveTodoList() {
+		String temporaryUserId = "temporary-user"; //temporary user id
+
+		// (1) 서비스 메서드의 retrieve() 메서드를 사용해 Todo 리스트를 가져온다.
+		List<TodoEntity> entities = todoService.retrieve(temporaryUserId);
+
+		// (2) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+		// (3) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다.
+		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+		// (4) ResponseDTO를 리턴한다.
+		return ResponseEntity.ok().body(response);
+	}
 
 }
