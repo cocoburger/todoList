5c91a11bb9f32bd9c448db74d585a3796011cbb4
cocoburger <73986381+cocoburger@users.noreply.github.com>
2022-04-16 22:12:48 +0900

    TodoDTO를 TodoEntity로 변환해주는 메서드 추가

--- src/main/java/com/example/demo/dto/TodoDTO.java
+++ src/main/java/com/example/demo/dto/TodoDTO.java
@@ -14,10 +14,21 @@ public class TodoDTO {
 	private String id;
 	private String title;
 	private boolean done;
+
 	//보안상 userId는 숨기는 것이 맞다. 그래서 DTO에는 userId를 만들지 않는다.
 	public TodoDTO(final TodoEntity entity) {
 		this.id = entity.getId();
 		this.title = entity.getTitle();
 		this.done = entity.isDone();
+	}
+
+	//response시에 캡슐화하거나 추가적인 정보를 함께 반환하려고 DTO를 사용한다.
+	//컨트롤러는 사용자에게서 TodoDTO를 요청 바디로 넘겨받고 이를 TodoEntity로 변환해 저장해야한다.
+	public static TodoEntity todoEntity (TodoDTO dto) {
+		return TodoEntity.builder()
+				.id(dto.getId())
+				.title(dto.getTitle())
+				.done(dto.done)
+				.build();
 	}
 }
